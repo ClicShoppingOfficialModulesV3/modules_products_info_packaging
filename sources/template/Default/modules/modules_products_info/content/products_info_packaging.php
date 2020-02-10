51aff240067cb0a04bb4368cb2819f51299243dd
ClicShopping <Yae8mr2o*>
2020-02-10 11:00:15 -0500

    miss file

---
+++ sources/template/Default/modules/modules_products_info/content/products_info_packaging.php
@@ -0,0 +1,8 @@
+<div class="<?php echo $text_position; ?> col-md-<?php echo $content_width; ?>">
+  <div class="separator"></div>
+  <div style="float: <?php echo MODULE_PRODUCTS_INFO_PACKAGING_POSITION; ?>;">
+    <div class="modulesProductsInfoPackaging">
+      <span class="modulesProductsInfoPackaging"><h3><?php echo $products_packaging; ?></h3></span>
+    </div>
+  </div>
+</div>
