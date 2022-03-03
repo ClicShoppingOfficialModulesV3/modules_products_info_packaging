ec04533aa7a627eb267d1b8ea2ffc71e0ddea686
ClicShopping <admin@clicshopping.org>
2022-03-03 22:43:06 +0100

    update

--- sources/template/Default/modules/modules_products_info/pi_products_info_packaging.php
+++ sources/template/Default/modules/modules_products_info/pi_products_info_packaging.php
@@ -15,7 +15,7 @@
 
   class pi_products_info_packaging {
     public string $code;
-    public $group;
+    public string $group;
     public $title;
     public $description;
     public ?int $sort_order = 0;
